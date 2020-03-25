047864686e9befefc1c8aa984355866cf73c648a
hehao <hhe@novasky.cn>
2020-03-25 15:32:41 +0800

    公司电脑提交

--- src/main/java/com/hhe/graduate/controller/System/login.java
+++ src/main/java/com/hhe/graduate/controller/System/login.java
@@ -1,4 +1,12 @@
 package com.hhe.graduate.controller.System;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class login {
+    @GetMapping("/login")
+    public String login(){
+        return "user/login";
+    }
 }
